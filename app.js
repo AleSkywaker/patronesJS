72befc207d4f62b007042bd9ed189531c3c7018c
Alex Colombo <uricaine@hotmail.com>
2018-12-19 23:11:33 +0100

    init factory

--- app.js
+++ app.js
@@ -1,5 +1,23 @@
 //Factory patterns
 
-var peopleFactory = function() {
+var peopleFactory = function(name, age, state) {
   var temp = {};
+
+  temp.name = name;
+  temp.age = age;
+  temp.state = state;
+
+  temp.printPerson = function() {
+    console.log(`Tu nombre es ${name}, vives en ${state} y tienes ${age}`);
+  };
+
+  return temp;
 };
+
+var person1 = peopleFactory("Alex", 23, "Madrid");
+var person2 = peopleFactory("Luca", 33, "Barcelona");
+var person3 = peopleFactory("Patricia", 19, "Valencia");
+
+person1.printPerson();
+person2.printPerson();
+person3.printPerson();
